e85bfc32a79ebddbaa46aadf483adf29c7f7a719
Markus Haapamäki <markushaapa@gmail.com>
2018-11-19 16:39:29 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -83,3 +83,26 @@ Config.Teleports = {
 		} 
 	}
 }
+
+Config.Uniforms = {
+	prison_wear = {
+		male = {
+			['tshirt_1'] = 15,  ['tshirt_2'] = 0,
+			['torso_1']  = 146, ['torso_2']  = 0,
+			['decals_1'] = 0,   ['decals_2'] = 0,
+			['arms']     = 0,   ['pants_1']  = 3,
+			['pants_2']  = 7,   ['shoes_1']  = 12,
+			['shoes_2']  = 12,  ['chain_1']  = 50,
+			['chain_2']  = 0
+		},
+		female = {
+			['tshirt_1'] = 3,   ['tshirt_2'] = 0,
+			['torso_1']  = 38,  ['torso_2']  = 3,
+			['decals_1'] = 0,   ['decals_2'] = 0,
+			['arms']     = 2,   ['pants_1']  = 3,
+			['pants_2']  = 15,  ['shoes_1']  = 66,
+			['shoes_2']  = 5,   ['chain_1']  = 0,
+			['chain_2']  = 2
+		}
+	}
+}
