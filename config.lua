49034929e05072e336a42aadce5f961d1d18cf30
Fores <64091692+GurraG004@users.noreply.github.com>
2022-03-13 15:38:01 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,7 @@
 Config = {}
 
+Config.Policejob = 'police'
+
 Config.JailPositions = {
 	["Cell"] = { ["x"] = 1799.8345947266, ["y"] = 2489.1350097656, ["z"] = -119.02998352051, ["h"] = 179.03021240234 }
 }
